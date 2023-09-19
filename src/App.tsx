357d3330f8be3f3b5302f745a8faf23ce796c15a
AbsolutOli <vocameoli@gmail.com>
2023-09-19 20:50:36 +0300

    Added deadBlock

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { TextBlock } from "./components/TextBlock";
 import MainImage from "./assets/img/Group 1000002736.png";
+import DealBackImg from "./assets/img/bottomimg.jpg";
 
 function App() {
   return (
     <div className="wrapper">
-      <TextBlock />
-      <div className="mainimage">
-        <img width={700} src={MainImage} alt="" />
+      <div className="container">
+        <TextBlock />
+        <div className="mainimage">
+          <img width={700} src={MainImage} alt="" />
+        </div>
+        <div className="dealBlock">
+          <div className="dealBlock__body">
+            <img src={DealBackImg} alt="Deal block background" />
+            <span className="icon-deal-converted"></span>
+          </div>
+        </div>
+        <div className="sliderBlock">
+          <div className="sliderBlock__top">
+            <div className="sliderBlock__title">
+              <h3> בעלי מקצוע</h3>
+              <span className="icon-negotiation-converted"></span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/TextBlock/index.tsx
+++ src/components/TextBlock/index.tsx
@@ -1,33 +1,38 @@
 import { DropdownMenu } from "../DropdownMenu";
+import styles from "./TextBlock.module.scss";
+import clsx from "clsx";
 
 export const TextBlock: React.FC = () => {
   return (
-    <div className="textBlock">
-      <h2 className="textBlock__title">
-        !הגדל את המכירות שלך<span> מתווך?</span>
-      </h2>
-      <p className="textBlock__text">
-        באמצעות טכנולוגיה חדשנית המאפשרת לבנות מודל תלת ממדי של הנכס, אתר GetKey
-        חולה בתחום השיווק הדיגיטלי ומצעיר אתכם לעולם חדשני רת הרבה יותר מהיום
-        תוכלו לספק ללקוחותיכם חווית קנייה, מכירה או השכרה סוחפת מרק תמונות, עם
-        סיור וירטואלי שמזמן הפך לכלי שיווק נוח, קל ומשתלם מאי פעם למידע נוסף
-      </p>
-      <div className="textBlock__moreInfo moreInfo">
-        <div className="moreInfo__getInfo">
-          <section>
-            <input id="moreInfo__check" type="checkbox" />
-            <label htmlFor="moreInfo__check"> למידע נוסף</label>
-            <DropdownMenu />
-          </section>
-          <p className="moreInfo__info"> להזמנת צילום ופרסום חיים</p>
-          <a href="tel:*9983" className="moreInfo__number">
-            *9983
+    <div className={styles.textBlock}>
+      <div className={styles.textBlock__conteiner}>
+        <h2 className={styles.textBlock__title}>
+          <span> מתווך?</span> הגדל את המכירות שלך!
+        </h2>
+        <p className={styles.textBlock__text}>
+          באמצעות טכנולוגיה חדשנית המאפשרת לבנות מודל תלת ממדי של הנכס, אתר
+          GetKey חולה בתחום השיווק הדיגיטלי ומצעיר אתכם לעולם חדשני רת הרבה יותר
+          מהיום תוכלו לספק ללקוחותיכם חווית קנייה, מכירה או השכרה סוחפת מרק
+          תמונות, עם סיור וירטואלי שמזמן הפך לכלי שיווק נוח, קל ומשתלם מאי פעם
+          למידע נוסף
+        </p>
+        <div className={clsx(styles.textBlock__moreInfo, styles.moreInfo)}>
+          <div className={styles.moreInfo__getInfo}>
+            <section className={styles.moreInfo__dropdown}>
+              <input id="moreInfo__check" type="checkbox" />
+              <label htmlFor="moreInfo__check"> למידע נוסף</label>
+              <DropdownMenu />
+            </section>
+            <p className={styles.moreInfo__info}> להזמנת צילום ופרסום חיים</p>
+          </div>
+          <a href="tel:*9983" className={styles.moreInfo__number}>
+            9983*
           </a>
         </div>
+        <div className={styles.textBlock__buttomText}>
+          <p>לורם איפסוםוח איבן איף, ברומץ כלרשט מיחוצים. קלאצי קר</p>
+        </div>
       </div>
-      <p className="textBlock__buttomText">
-        לורם איפסוםוח איבן איף, ברומץ כלרשט מיחוצים. קלאצי קר
-      </p>
     </div>
   );
 };
